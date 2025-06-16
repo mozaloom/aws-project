cef6e3a26f5082e9decdfe3864405572b3827a4b
mozaloom <mzlwm320@gmail.com>
2025-06-16 22:59:38 +0300

    initial commit

---
+++ LambdaApiWithDynamo/ProductApi/Function.cs
@@ -0,0 +1,66 @@
+﻿using System.Text.Json;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace ProductApi;
+
+public class Function
+{
+    private static readonly IAmazonDynamoDB Client =   
+        new AmazonDynamoDBClient(
+            Environment.GetEnvironmentVariable("AccessKey"),
+            Environment.GetEnvironmentVariable("SecretKey"),
+        new AmazonDynamoDBConfig
+        {
+            RegionEndpoint = Amazon.RegionEndpoint.EUWest1
+        });
+    
+
+    private static readonly Table Table = new TableBuilder(Client, "Products")
+        .AddHashKey("ProductId", DynamoDBEntryType.String)
+        .Build();
+    
+    public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest request)
+    {
+        Console.WriteLine("FunctionHandler called");
+        
+        var product = JsonSerializer.Deserialize<Product>(request.Body);
+
+        if (product == null)
+        {
+            return new APIGatewayProxyResponse
+            {
+                StatusCode = 400,
+                Body = "Invalid product data."
+            };
+        }
+        
+        var document = new Document
+        {
+            ["ProductId"] = product.ProductId,
+            ["Name"] = product.Name,
+            ["Price"] = product.Price
+        };
+        
+        await Table.PutItemAsync(document);
+
+        Console.WriteLine("Product added successfully!");
+        
+        return new APIGatewayProxyResponse
+        {
+            StatusCode = 200,
+            Body = "Product added successfully!"
+        };
+    }
+}
+
+public class Product
+{
+    public string ProductId { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # aws-project
+# Building Serverless APIs with AWS Lambda and DynamoDB in .NET
+
+## Content
+
+-   YouTube: <https://youtu.be/Bz1lyyRgr3U>
